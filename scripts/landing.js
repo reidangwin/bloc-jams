12c8af14725ff2f340a5f481bde4bde63df07aa0
Reid Angwin <rangwin@gmail.com>
2017-08-02 05:54:01 -0400

    16 jQuery Landing Page: Completed

--- scripts/landing.js
+++ scripts/landing.js
@@ -1,30 +1,23 @@
-var pointsArray = document.getElementsByClassName('point');
-
-var animatePoints = function (points) {
-    var revealPoint = function (points) {
-        maxIndex = points.length;
-        var myCallback = function(item) {
-            item.style.opacity = 1;
-            item.style.transform = "scaleX(1) rotate(360deg) translateY(0)";
-            item.style.msTransform = "scaleX(1) rotate(360deg) translateY(0)";
-            item.style.WebkitTransform = "scaleX(1) rotate(360deg) translateY(0)";
-        };
-        forEach(points, myCallback);
+var animatePoints = function () {
+    var revealPoint = function () {
+        $(this).css({
+            opacity: 1,
+            transform: 'scaleX(1) translateY(0)'
+        });
     };
-
-    revealPoint(points);
+    $.each($('.point'), revealPoint())
 };
 
-window.onload = function () {
+$(window).load = (function () {
     // Automatically animate the points on a tall screen where scrolling won't trigger the animation
-    if (window.innerHeight > 950 ) {
-        animatePoints(pointsArray);
+    if ($(window).height() > 950 ) {
+        animatePoints();
     }
-    var sellingPoints = document.getElementsByClassName('selling-points')[0];
-    var scrollDistance = sellingPoints.getBoundingClientRect().top - window.innerHeight + 200;
-    window.addEventListener('scroll', function (event) {
-        if (document.documentElement.scrollTop || document.body.scrollTop >= scrollDistance) {
-            animatePoints(pointsArray);
+    var scrollDistance = $('selling-points').offset().top - $(window).height() + 200;
+
+    $(window).scroll(function (event) {
+        if ($(window).scrollTop() >= scrollDistance) {
+            animatePoints();
         }
     });
-};
+});
